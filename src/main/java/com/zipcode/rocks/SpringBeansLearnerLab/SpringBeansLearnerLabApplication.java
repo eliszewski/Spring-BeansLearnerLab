368096282622540a00a76836c4a21746bdbdebdd
eliszewski <liszewski31098@gmail.com>
2022-12-18 19:59:26 -0500

    setup

---
+++ src/main/java/com/zipcode/rocks/SpringBeansLearnerLab/SpringBeansLearnerLabApplication.java
@@ -0,0 +1,13 @@
+package com.zipcode.rocks.SpringBeansLearnerLab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBeansLearnerLabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBeansLearnerLabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zipcode/rocks/SpringBeansLearnerLab/SpringBeansLearnerLabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zipcode.rocks.SpringBeansLearnerLab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBeansLearnerLabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
